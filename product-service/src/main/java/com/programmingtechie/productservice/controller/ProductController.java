23d37c3da205551be0cb0615c70693ab80e01587
Shourov Foisal <shourovfoisal@gmail.com>
2023-04-15 23:08:09 +0600

    Adding the product service codes (unfinished)

---
+++ product-service/src/main/java/com/programmingtechie/productservice/controller/ProductController.java
@@ -0,0 +1,17 @@
+package com.programmingtechie.productservice.controller;
+
+import com.programmingtechie.productservice.dto.ProductRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/product")
+public class ProductController {
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest) {
+
+    }
+
+}
