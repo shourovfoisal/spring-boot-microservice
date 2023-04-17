d03269bc5f58e91ad665f80a011d881fa85e72a7
Shourov Foisal <shourovfoisal@gmail.com>
2023-04-17 09:39:56 +0600

    Completed the inventory service

--- inventory-service/src/main/java/com/programmingtechie/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/programmingtechie/inventoryservice/InventoryServiceApplication.java
@@ -24,7 +24,10 @@ public class InventoryServiceApplication {
 			Inventory inventory1 = new Inventory();
 			inventory1.setSkuCode("iphone_13_red");
 			inventory1.setQuantity(0);
-		}
+
+			inventoryRepository.save(inventory);
+			inventoryRepository.save(inventory1);
+		};
 	}
 
 }
